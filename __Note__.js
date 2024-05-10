a2209785880a4033316c1f9174782eb46d2cb64b
tanvir244 <tr30365s25@gmail.com>
2024-05-11 03:50:01 +0600

    server setup complete

---
+++ __Note__.js
@@ -0,0 +1,9 @@
+/**
+ * =====================
+ *     Server Setup
+ * =====================
+ * 1. npm init -y
+ * 2. npm i express cors mongodb dotenv
+ * 3. than open the project with code editor and than go to package.json file and add "start": "node index.js" in the scripts object
+ * 4. than create an index.js file
+ */
