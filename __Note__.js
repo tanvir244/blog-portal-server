5eccb91a09cb8bee3fdb27c861b4ef88d9a4ba48
tanvir244 <tr30365s25@gmail.com>
2024-05-11 04:08:30 +0600

    server connect to mongoDB complete

--- __Note__.js
+++ __Note__.js
@@ -6,4 +6,5 @@
  * 2. npm i express cors mongodb dotenv
  * 3. than open the project with code editor and than go to package.json file and add "start": "node index.js" in the scripts object
  * 4. than create an index.js file
+ * Note: know more about- [Module:59-1 Project And Set Basic Server]
  */
