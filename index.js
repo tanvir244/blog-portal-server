9b1bc9cdfc779bf5a0dc7ac6a235134db6bead58
tanvir244 <tr30365s25@gmail.com>
2024-05-12 03:44:59 +0600

    comment related api creation

--- index.js
+++ index.js
@@ -9,9 +9,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-console.log(process.env.DB_USER)
-console.log(process.env.DB_PASS)
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9nu6wnq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -28,6 +25,7 @@ async function run() {
 
     const recentCollection = client.db('blogPortal').collection('recentBlogs');
     const addedCollection = client.db('blogPortal').collection('addedBlogs');
+    const commentCollection = client.db('blogPortal').collection('allComments');
 
     app.get('/recent_blogs', async(req, res) => {
         const cursor = recentCollection.find();
@@ -62,6 +60,23 @@ async function run() {
       const id = req.params.id;
       const query = {_id: new ObjectId(id)};
       const result = await addedCollection.findOne(query);
+      res.send(result);
+    })
+
+    app.post('/all_comments', async(req, res) => {
+      const newComment = req.body;
+      const result = await commentCollection.insertOne(newComment);
+      res.send(result);
+    })
+
+    app.get('/all_comments', async(req, res) => {
+      const cursor = commentCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    app.get('/all_comments/:id', async(req, res) => {
+      const result = await commentCollection.find({commentId: req.params.id}).toArray();
       res.send(result);
     })
 
